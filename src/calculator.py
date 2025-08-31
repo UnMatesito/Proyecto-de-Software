651f63ea28fc6c078fb5c7dcb2e3a9ad3053e45d
Tobias Palumbo <palumbotobias@gmail.com>
2025-08-31 13:44:36 -0300

    agregando division

--- src/calculator.py
+++ src/calculator.py
@@ -9,4 +9,10 @@ def suma(a, b):
 def multiplicacion(a, b):
     return a * b
 
-    
+def division(a, b):
+    try:
+        result = a / b
+        return result
+    except ZeroDivisionError:
+        print("No podes divir por 0")
+        return None
