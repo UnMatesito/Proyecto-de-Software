408138fac96712a7ee60cd89fa65e7dfc8d7ab69
Neftalí Taiel Toledo Dicroce <toledoneftali@hotmail.com>
2025-09-05 23:45:14 +0000

    Mover calculadora a directorio propio

---
+++ calculator/src/calculator.py
@@ -0,0 +1,18 @@
+# TODO
+
+def resta(a, b):
+    return a - b
+
+def suma(a, b):
+    return a + b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    try:
+        result = a / b
+        return result
+    except ZeroDivisionError:
+        print("No podes divir por 0")
+        return None
