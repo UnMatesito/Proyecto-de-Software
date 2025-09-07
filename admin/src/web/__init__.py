46236fe577ff6f082b8783631331c38a052c0a20
Cielo <vegacielo53@gmail.com>
2025-09-07 01:43:04 +0000

    agrego primer controlador

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 def create_app(env="development", static_folder="../../static"):
     app = Flask(__name__, static_folder=static_folder)
     
-    # TODO
+    @app.route('/')
+    def home():
+        return '¡Hola, Mundo!'
 
     return app
