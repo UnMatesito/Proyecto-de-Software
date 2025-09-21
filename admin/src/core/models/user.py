52c8799ac80d27d612e60918738f294cc379015c
Carlos Benitez <carlos_andres01.10@hotmail.com>
2025-09-21 15:26:32 +0000

    corrigo poetry

--- admin/src/core/models/user.py
+++ admin/src/core/models/user.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone
-
+import bcrypt
 from core.database import db
 
 
@@ -29,7 +29,10 @@ class User(db.Model):
         onupdate=lambda: datetime.now(timezone.utc),
         nullable=False,
     )
+    """Funcion propia para setear una contraseña con bcryp, "raw_password: str" NO obliga a que el parametro sea un str, es solo una notacion y ayuda a detectar errores    """
+    def set_password(self, new_password: str):
+        password_hashed = bcrypt.hashpw(new_password.encode("utf-8"), bcrypt.gensalt()) #Creo el hash de la password
+        self.password_hash= password_hashed.decode("utf-8") #La guardo en un string legible 
+
     def __repr__(self):
         return f"<User {self.first_name}>"
-
-prueba 
