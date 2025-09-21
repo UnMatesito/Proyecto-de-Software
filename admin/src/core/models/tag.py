d101d286aff25ad402c0c003affdd9ed27fc9d0f
Carlos Benitez <carlos_andres01.10@hotmail.com>
2025-09-21 02:39:25 +0000

    agrego la "t" de created_at de tag

--- admin/src/core/models/tag.py
+++ admin/src/core/models/tag.py
@@ -6,7 +6,7 @@ class Tag(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(50), nullable = False)
     slug = db.Column(db.String(50), nullable = False)
-    created_a = db.Column(
+    created_at = db.Column(
         db.DateTime, 
         default = lambda: datetime.now(timezone),
         nullable = False)   
