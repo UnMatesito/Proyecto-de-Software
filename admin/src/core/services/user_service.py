829b7bc437d59e3b9798cee5c3b79b8b5a720a8b
Carlos Benitez <carlos_andres01.10@hotmail.com>
2025-09-21 14:47:56 +0000

    Agrego dependencia bcrypt

--- admin/src/core/services/user_service.py
+++ admin/src/core/services/user_service.py
@@ -15,7 +15,7 @@ def create_user(**kwargs):
     db.session.commit()
     return user
 """
-Cuando cree los serv de deleted y block, me di cuenta que es casi el mismo codigo, tonces lo reciclo en esta funcion que hace un update de x atributo 
+Cuando cree los serv de deleted y block, me di cuenta que es casi el mismo codigo, entonces lo reciclo en esta funcion que hace un update de x atributo 
 Le llega user_id para checkear que exista el user, el atributo a modificar, el valor, y la funcion que verifica el estado del atributo 
 """
 def update_user_attribute(user_id, attr_name, new_value, check_func=None):
