a6bd30b7f48e70aba67e1d98bc1397176c68b2a7
Neftalí Taiel Toledo Dicroce <toledoneftali@hotmail.com>
2025-09-21 15:07:36 +0000

    Elimino modelos de ejemplo

--- admin/src/core/services/label_service.py
+++
@@ -1,9 +0,0 @@
-from core.database import db
-from core.models import Label
-
-
-def create_label(name):
-    label = Label(name=name)
-    db.session.add(label)
-    db.session.commit()
-    return label
