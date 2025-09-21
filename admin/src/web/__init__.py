702ffb598dcbcdb72cc7897a26ebf87c93763fc7
Neftalí Taiel Toledo Dicroce <toledoneftali@hotmail.com>
2025-09-21 15:07:12 +0000

    Arreglo de configuracion para prod

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -3,14 +3,13 @@ from flask_debugtoolbar import DebugToolbarExtension
 from core.utils.bcrypt import bcrypt
 from core.database import db
 
-from .config import current_config
+from .config import get_current_config
 from .handlers import error
 
 
-# TODO: Hay que usar esta variable "env"
 def create_app(env="development", static_folder="../../static"):
     app = Flask(__name__, static_folder=static_folder)
-    app.config.from_object(current_config)
+    app.config.from_object(get_current_config(env))
 
     if app.config.get("DEBUG", False):
         DebugToolbarExtension(app)
