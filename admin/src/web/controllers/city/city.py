7154d01cdb9c0ae99e66e58290348ac88be166e8
Tobias Palumbo <palumbotobias@gmail.com>
2025-10-07 15:57:13 +0000

    agregando manejador por si no encunetra se encuentra la prov al buscar sus ciudades

--- admin/src/web/controllers/city/city.py
+++ admin/src/web/controllers/city/city.py
@@ -1,13 +1,6 @@
 from flask import (
     Blueprint,
-    Response,
-    flash,
     jsonify,
-    redirect,
-    render_template,
-    request,
-    session,
-    url_for,
 )
 
 from core.services import get_province_by_id
@@ -17,5 +10,8 @@ city_bp = Blueprint("city_bp", __name__, url_prefix="/cities")
 
 @city_bp.get("/<int:province_id>")
 def get_cities(province_id):
-    province = get_province_by_id(province_id)
-    return jsonify([{"id": c.id, "name": c.name} for c in province.cities])
+    try: 
+        province = get_province_by_id(province_id)
+        return jsonify([{"id": c.id, "name": c.name} for c in province.cities])
+    except Exception as e :
+         return jsonify([{"id": "", "name": ""}])
