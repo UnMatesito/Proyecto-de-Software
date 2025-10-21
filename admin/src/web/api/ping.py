42556f91f23247cc5b88d76af2bd29c863ed2989
Neftalí Taiel Toledo Dicroce <toledoneftali@hotmail.com>
2025-10-21 22:05:22 +0000

    Elimino ping

--- admin/src/web/api/ping.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify, Blueprint
-
-api_bp = Blueprint('api', __name__, url_prefix='/api')
-
-
-@api_bp.route('/ping', methods=['GET'])
-def ping():
-    return jsonify({'message': 'pong'}), 200
