7f88f9e7f87efe6a1f657975a05017cfbfbf4751
Tobias Palumbo <palumbotobias@gmail.com>
2025-10-22 11:37:47 +0000

    agregano iconos para la card de sitios

--- portal/src/components/Card.vue
+++ portal/src/components/Card.vue
@@ -1,6 +1,7 @@
 <script setup>
     import IconFavorite from './icons/IconFavorite.vue'
     import IconLocation from './icons/IconLocation.vue'
+    import IconBuild from './icons/IconBuild.vue'
     const props = defineProps(["name", "province", "city", "tags", "state_of_conservation", "inauguration_year", "category", "imagen"])
     const tags_to_show = props.tags.slice(0, 5)
     const tags_left = props.tags.length > 5 ? props.tags.length - 5 : 0
@@ -9,13 +10,18 @@
 </script>
 
 <template>
-    <div class=" rounded-lg relative flex flex-col gap-1 overflow-hidden max-w-64 cursor-pointer shadow-lg">
+    <div class=" rounded-lg relative flex flex-col gap-1 overflow-hidden md:max-w-64 cursor-pointer shadow-lg">
         <span class="absolute left-1 top-1 bg-slate-200 p-1.5 border-0 rounded-full w-9 fill-gray-500 text-center hover:opacity-75 hover:fill-red-700 transition-opacity duration-400 ease-in-out"> 
             <IconFavorite class=""></IconFavorite>
         </span>
         <img class="object-cover" :src="urlImg" :alt ="alt">
         <div class="p-2 flex flex-col gap-1">
-            <h3 class="font-semibold"><i class="fa-solid fa-building-columns  text-yellow-500"></i> {{ props.name }}</h3>
+            <div class="flex gap-1 items-center">
+                <IconBuild class="fill-yellow-500 w-4"></IconBuild>
+                <h3 class="font-semibold">
+                    {{ props.name }}
+                </h3>
+            </div>
             <div>
                 <span class=" font-sans flex items-center gap-1 ">
                     <IconLocation class="fill-red-700 w-3"></IconLocation>
@@ -25,11 +31,11 @@
             <div class="flex gap-1 justify-between">
                 <div>
                     <span class="text-gray-500 text-sm font-semibold">Estado</span>
-                    <p value="" class="text-gray-700 text-sm font-semibold">{{ props.state_of_conservation }}</p>
+                    <p  class="text-gray-700 text-sm font-semibold">{{ props.state_of_conservation }}</p>
                 </div>
                 <div>
                     <span class="text-gray-500 text-sm font-semibold">Año</span>
-                    <p value="" class="text-gray-700 text-sm font-semibold">{{ props.inauguration_year }}</p>
+                    <p  class="text-gray-700 text-sm font-semibold">{{ props.inauguration_year }}</p>
                 </div>
                 <div>
                     <span class="text-gray-500 text-sm font-semibold">Categoria</span>
@@ -37,8 +43,8 @@
                 </div>
             </div>
             <div class="flex gap-2 text-blue-700 flex-wrap border-t-2 pt-1">
-                <span  v-for="tag in tags_to_show" class="inline-flex items-center bg-blue-300 text-sky-950 text-xs font-bold px-2.5 py-0.5 border-2 rounded-full ">{{ tag }}</span>
-                <span  v-if="tags_left > 0" class="inline-flex items-center bg-blue-300 text-sky-950 text-xs font-bold px-2.5 py-0.5 border-2 rounded-full ">{{ `+${tags_left}` }}</span>
+                <span v-for="tag in tags_to_show" class="inline-flex items-center bg-blue-300 text-sky-950 text-xs font-bold px-2.5 py-0.5 border-2 rounded-full ">{{ tag }}</span>
+                <span v-if="tags_left > 0" class="inline-flex items-center bg-blue-300 text-sky-950 text-xs font-bold px-2.5 py-0.5 border-2 rounded-full ">{{ `+${tags_left}` }}</span>
             </div>
         </div>
     </div>
