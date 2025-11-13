ae201d6e7a716a6c2d8f40643d0ea101be68f37c
Neftalí Taiel Toledo Dicroce <toledoneftali@hotmail.com>
2025-11-13 14:49:46 +0000

    Para verificar las envs

--- admin/app.py
+++ admin/app.py
@@ -2,4 +2,8 @@ from web import create_app
 
 if __name__ == "__main__":
     app = create_app()
+    import os
+
+    for key, value in os.environ.items():
+        print(f"{key} = {value}")
     app.run()
