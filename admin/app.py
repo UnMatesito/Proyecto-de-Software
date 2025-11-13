c689d9b0114d12adc696348f630c8249a9f83926
Neftalí Taiel Toledo Dicroce <toledoneftali@hotmail.com>
2025-11-13 14:56:56 +0000

    Saco esto

--- admin/app.py
+++ admin/app.py
@@ -2,8 +2,4 @@ from web import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    import os
-
-    for key, value in os.environ.items():
-        print(f"{key} = {value}")
     app.run()
