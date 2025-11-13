148cef8f61d727eba44bcdec021f4412bd4e13ac
Neftalí Taiel Toledo Dicroce <toledoneftali@hotmail.com>
2025-11-13 20:53:00 +0000

    Remuevo caché del feature flag para que sea más instantáneo

--- portal/src/api/featureFlags.js
+++ portal/src/api/featureFlags.js
@@ -1,24 +1,7 @@
 import api from "@/api/axios.js";
 
-const cache = {};
-const ongoing = {};
-
 export const getFeatureFlag = async (name) => {
-  // Si ya está en caché, devolverla sin llamar a la API
-  if (cache[name]) return cache[name];
+  const res = await api.get(`/feature-flags/${name}`)
 
-  // Si ya se está pidiendo, devolver la misma promesa
-  if (ongoing[name]) return ongoing[name];
-
-  // Si no existe en caché ni hay request en curso, pedirla
-  ongoing[name] = api.get(`/feature-flags/${name}`)
-    .then((res) => {
-      cache[name] = res.data;
-      return cache[name];
-    })
-    .finally(() => {
-      delete ongoing[name];
-    });
-
-  return ongoing[name];
+  return res.data;
 };
