2fba2ea1c779455e23667494ed46c3e3f36e6bbe
Neftalí Taiel Toledo Dicroce <toledoneftali@hotmail.com>
2025-11-16 21:11:03 +0000

    Fixeo avatares y reseña propia

--- portal/src/components/Avatar.vue
+++ portal/src/components/Avatar.vue
@@ -1,16 +1,27 @@
 <template>
+  <div class="flex items-center gap-2">
+    <!-- Si hay avatar, mostrar imagen -->
+    <img
+      :src="avatar"
+      :alt="name"
+      class="w-9 h-9 rounded-full object-cover"
+    />
 
-<div class="flex items-center gap-2">
-    <IconAvatar class="fill-proyecto-primary w-9"></IconAvatar>
+
     <div class="font-medium dark:text-white">
-        <div>{{ name }}</div>
-        <div class="text-xs sm:text-sm text-gray-500 dark:text-gray-400">{{ email }}</div>
+      <div>{{ name }}</div>
+      <div class="text-xs sm:text-sm text-gray-500 dark:text-gray-400">{{ email }}</div>
     </div>
-</div>
-
+  </div>
 </template>
 
 <script setup>
-    import IconAvatar from './icons/IconAvatar.vue';
-    defineProps(["name", "email"])
+defineProps({
+  name: String,
+  email: String,
+  avatar: {
+    type: String,
+    default: null
+  }
+})
 </script>
